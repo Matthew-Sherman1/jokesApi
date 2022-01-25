5366977dc7b5c037d34f53dec68a8c8ff6a56f1d
Matthew-Sherman1 <93622077+Matthew-Sherman1@users.noreply.github.com>
2022-01-24 19:08:28 -0700

    initial commit

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
-	joke: {type: String},
-	answer: {type: String}
+	joke: String,
+	punchline: String
 });
 
 const Joke = mongoose.model("Joke", JokeSchema);
